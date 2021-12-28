6cf5705be3e1d1ba693b6ff85ba101985f98088e
ppinner <56121914+ppinner@users.noreply.github.com>
2021-12-28 12:16:45 +0000

    Initial commit

---
+++ src/main/java/com/example/take5backend/Take5BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.take5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Take5BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Take5BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/take5backend/Take5BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.take5backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Take5BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
