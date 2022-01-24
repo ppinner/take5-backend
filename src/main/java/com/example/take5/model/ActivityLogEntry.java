1de36aea40919b562e27fb899d74e4a9d670050c
ppinner <56121914+ppinner@users.noreply.github.com>
2022-01-24 14:29:41 +0000

    adding reflection and rating to activity log object

--- src/main/java/com/example/take5/model/ActivityLogEntry.java
+++ src/main/java/com/example/take5/model/ActivityLogEntry.java
@@ -1,19 +1,26 @@
 package com.example.take5.model;
 
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 
 public class ActivityLogEntry {
     Activity activity;
+    @Id
     String id;
+    String reflection;
+    Integer rating;   //0-5 based on emotion OR -2 to 2?
     Date date;
 
     public ActivityLogEntry() {
         super();
     }
 
-    public ActivityLogEntry(Activity activity, String id, Date date) {
+    public ActivityLogEntry(Activity activity, String id, Date date, String reflection, Integer rating) {
         this.activity = activity;
         this.id = id;
+        this.reflection = reflection;
+        this.rating = rating;
         this.date = date;
     }
 
@@ -35,5 +42,21 @@ public class ActivityLogEntry {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public String getReflection() {
+        return reflection;
+    }
+
+    public void setReflection(String reflection) {
+        this.reflection = reflection;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
     }
 }
